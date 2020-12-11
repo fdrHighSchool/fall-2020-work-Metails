a1aba55c962eb50478649d40af06c6d4b155c670
Metails <dengjianhua2397462613#gmail.com>
2020-12-11 08:58:13 -0500

    added grades work for median and mode

---
+++ 09-Advanet of Code/2019_Code/Part2.java
@@ -0,0 +1,14 @@
+public class Part2.java{
+  public static void main(String[] args){
+
+  }//end main method
+}//end class
+
+
+/*
+Plan!
+I'm still unsure of what part 2 is asking but I'll assume it's asking for number that add to zero
+Get the list of the input puzzle ready
+Use those number to calculate the results by creating while loops
+while it's not = to 0 it will continue on trying
+*/
