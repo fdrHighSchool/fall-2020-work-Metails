9e01613eb4adf05d6933992c781d0616f56b4f86
Metails <dengjianhua2397462613#gmail.com>
2021-01-13 09:20:46 -0500

    Upadted work

--- 14-My_first_class/Anime_Opinions.java
+++ 14-My_first_class/Anime_Opinions.java
@@ -13,28 +13,29 @@ public Anime_Opinions(String anime1, String anime2, String anime3){
 }//end Anime_Weebs constructor method
 
 public void displayAnimeChoices(){
-  System.out.println(this.animeChoice1);
-  System.out.println(this.animeChoice2);
-  System.out.println(this.animeChoice3);
+  System.out.println("1." + this.animeChoice1);
+  System.out.println("2." + this.animeChoice2);
+  System.out.println("3." + this.animeChoice3);
 }//end displayFinalAnime method
 
 public void personsChoice(){
-  String favorite;
   Scanner prefered = new Scanner(System.in);
   System.out.println("Which one do you prefered? Chooses starting from your favorite then to your least favorite by picking 1, 2 or 3: ");
   String playerChoice = prefered.nextLine();
 
-  if(playerChoice.equals(1)){
-  favorite = this.animeChoice1;
-  }if(playerChoice.equals(2)){
-  favorite = this.animeChoice2;
-  }if(playerChoice.equals(3)){
-  favorite = this.animeChoice3;
+  if(playerChoice.equals("1")){
+  this.favorite = this.animeChoice1;
+  }
+  if(playerChoice.equals("2")){
+  this.favorite = this.animeChoice2;
+  }
+  if(playerChoice.equals("3")){
+  this.favorite = this.animeChoice3;
   }//last bracket of above if statement
 
 }//end personsChoice method
 
-public String weebsChoice(){
+public String toString(){
   return "His favorite Anime is: " + favorite;
 }//end weebsChoice
 
